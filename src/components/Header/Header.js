9737d016350292abbbd581aa82ee0659291996f5
Nayon <nayonroy64@gmail.com>
2022-10-27 19:16:40 +0600

    add footer section and navbar background color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 const Header = () => {
 	
 	return (
-		<div>
+		<div className='bg-rose-100'>
 			{/* nav bar section */}
 			<Navbar></Navbar>
 		</div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className="">
-      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
 
         {/* this navbar active in large device */}
         <div className="relative flex items-center justify-between">
