c7f0416c786590f8d3b2b1b4ab0096f75671d846
Nayon <nayonroy64@gmail.com>
2022-10-27 08:59:18 +0600

    add 404 error page

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Error404 = () => {
 	return (
 		<div>
-			<h1>No page found</h1>
+			<div className='flex align-middle justify-center h-screen'>
+                <div className='max-w-md my-auto'>
+                  <h1 className='text-9xl font-bold text-rose-700'>Oops!</h1>
+                  <p className='font-semibold text-2xl mt-8 text-gray-700'>404 - PAGE NOT FOUND</p>
+                  <p className='max-w-md mt-3'>The page your are looking for might have been romoved had its name changed or is temporarily unavaliable.</p>
+                </div>
+                
+              </div>
 		</div>
 	);
 };
