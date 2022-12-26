2edbe76b659066d1b768e8311a49ccda885b5f8c
Nayon <nayonroy64@gmail.com>
2022-12-26 19:00:14 +0600

    add feedback

--- src/pages/Home/Feedback/Feedback.js
+++ src/pages/Home/Feedback/Feedback.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Feedback = () => {
+
+  const feedbacks=[{name:"Nitol Asa",message:"Hello world."}]
+
+
 	return (
     <div className="my-12 max-w-6xl mx-auto ">
       <h1 className="text-4xl font-bold text-gray-600 text-center my-4 mb-6">
